bb303846c717b44e1e10ef9c2c696d697d523ba5
Aaron Vance <av4625@gmail.com>
2021-10-14 23:16:10 +0100

    Add constrict function to calculations

--- src/mathmatics/calculations.hpp
+++ src/mathmatics/calculations.hpp
@@ -15,6 +15,12 @@ public:
         float max_in_range,
         int min_out_range,
         int max_out_range) const;
+
+    template<typename T>
+    T constrict(T value, T minimum, T maximum) const
+    {
+        return (value < minimum) ? minimum : ((value > maximum) ? maximum : value);
+    }
 };
 
 }
--- test/mathmatics/calculations_tests.cpp
+++ test/mathmatics/calculations_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include <mathmatics/calculations.hpp>
 
 namespace mathmatics
@@ -8,18 +10,48 @@ namespace mathmatics
 namespace
 {
 
-class CalculationsTests : public ::testing::TestWithParam<
-    std::tuple<float, float, float, float, int, int> >
+class CalculationsTestsBase : public ::testing::Test
 {
 protected:
-    CalculationsTests() : calc_() {}
+    CalculationsTestsBase() : calc_() {}
 
     calculations calc_;
 };
 
+class CalculationsTestsMap :
+    public CalculationsTestsBase,
+    public ::testing::WithParamInterface<
+        std::tuple<float, float, float, float, int, int> >
+{
+};
+
+template<class T>
+class CalculationsTestsConstrict : public ::testing::Test
+{
+protected:
+    static std::vector<T> values_;
+};
+
+typedef ::testing::Types<
+    std::tuple<int, int, int, int>,
+    std::tuple<float, float, float, float> > TestTypes;
+
+template<> std::vector<std::tuple<int, int, int, int> > CalculationsTestsConstrict<std::tuple<int, int, int, int> >::values_{
+    std::make_tuple(1, 1, 1, 10),
+    std::make_tuple(1, 0, 1, 10),
+    std::make_tuple(10, 10, 1, 10),
+    std::make_tuple(10, 11, 1, 10),
+    std::make_tuple(5, 5, 1, 10)};
+template<> std::vector<std::tuple<float, float, float, float> > CalculationsTestsConstrict<std::tuple<float, float, float, float> >::values_{
+    std::make_tuple(1.1, 1.1, 1.1, 10.1),
+    std::make_tuple(1.1, 1.0, 1.1, 10.1),
+    std::make_tuple(10.1, 10.1, 1.1, 10.1),
+    std::make_tuple(10.1, 10.2, 1.1, 10.1),
+    std::make_tuple(5.1, 5.1, 1.1, 10.1)};
+
 }
 
-TEST_P(CalculationsTests, MapWillReturnCorrectValue)
+TEST_P(CalculationsTestsMap, MapWillReturnCorrectValue)
 {
     EXPECT_NEAR(
         std::get<0>(GetParam()),
@@ -34,7 +66,7 @@ TEST_P(CalculationsTests, MapWillReturnCorrectValue)
 
 INSTANTIATE_TEST_SUITE_P(
     Values,
-    CalculationsTests,
+    CalculationsTestsMap,
     ::testing::Values(
         std::make_tuple(4, 2, 0, 10, 0, 20),
         std::make_tuple(3, 1.5, 0, 10, 0, 20),
@@ -45,5 +77,26 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_tuple(1, 0.6, 0.5, 1.5, 0, 10),
         std::make_tuple(0.5, 0.55, 0.5, 1.5, 0, 10),
         std::make_tuple(-5, 5, 0, 10, 0, -10)));
+
+TYPED_TEST_SUITE_P(CalculationsTestsConstrict);
+
+TYPED_TEST_P(CalculationsTestsConstrict, ConstrictWillReturnCorrectValue)
+{
+    calculations calc;
+
+    for (TypeParam value : CalculationsTestsConstrict<TypeParam>::values_)
+    {
+        EXPECT_NEAR(
+            std::get<0>(value),
+            calc.constrict(
+                std::get<1>(value), std::get<2>(value), std::get<3>(value)),
+            0.1);
+    }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(
+    CalculationsTestsConstrict, ConstrictWillReturnCorrectValue);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(My, CalculationsTestsConstrict, TestTypes);
 
 }
